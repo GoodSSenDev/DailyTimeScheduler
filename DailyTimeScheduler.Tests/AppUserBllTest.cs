91e975a1b3a329b248261d4f1d5df981b1599c05
GoodSSenDev <yoodonggeun7@naver.com>
2020-12-16 11:37:59 +1300

    Added Register test on Business Logic Layer
    For unit testing and functions logics are correct and working correctly

--- DailyTimeScheduler.Tests/AppUserBllTest.cs
+++ DailyTimeScheduler.Tests/AppUserBllTest.cs
@@ -12,7 +12,7 @@ namespace DailyTimeScheduler.Tests
     /// This is test class for testing Async or sync function of Business logic about user using mocking for DB access.  
     /// </summary>
     [Collection("Sequential")]
-    public class UserBllTest 
+    public class AppUserBllTest 
     {
         /// <summary>
         /// Login Async Method  expected return an AppUser object that have different password. (hashed password)
@@ -27,7 +27,7 @@ namespace DailyTimeScheduler.Tests
             var mockDalClass = new Mock<IAppUserDal>();
             var realPassword = "qwer1234";
             mockDalClass.Setup(x => x.GetAppUserByIdAsync(It.IsAny<string>()))
-                .ReturnsAsync(new AppUser()
+                .ReturnsAsync( new AppUser()
                 {
                     No = 1,
                     Id = "test123",
@@ -46,6 +46,43 @@ namespace DailyTimeScheduler.Tests
             Assert.NotNull(returnAppUser);
 
             Assert.False(realPassword == returnAppUser.Password);
+        }
+        /// <summary>
+        /// Register Async Method expected check the duplication exist on unique field(Id, NickName) and 
+        /// return true if success 
+        /// </summary>
+        [Fact]
+        public async void RegisterAsync_ShouldReturnAppUserObject()
+        {
+            //preparing the Data Access Layer class that going in the Busineess Logic class
+            //Mocking
+            //1. Arrange
+            var mockDalClass = new Mock<IAppUserDal>();
+            //Checking Unique 
+            mockDalClass.Setup(x => x.GetAppUserByIdAsync(It.IsAny<string>()))
+                .ReturnsAsync((AppUser)(null));
+            mockDalClass.Setup(x => x.GetAppUserByNickNameAsync(It.IsAny<string>()))
+                .ReturnsAsync((AppUser)(null));
+            //Creating New User
+            mockDalClass.Setup(x => x.CreateNewAppUserAsync(It.IsAny<AppUser>()))
+                .ReturnsAsync(true);
+
+
+            var userDal = new AppUserBll(mockDalClass.Object);
+
+            //2. Act
+            var returnAppUser = await userDal.RegisterAsync( new AppUser()
+                {
+                    No = 1,
+                    Id = "test123",
+                    NickName = "nickDan",
+                    Password = "qwer1234",
+                    AccessLevel = 1
+                });
+
+            //3.Assert
+
+            Assert.True(returnAppUser.Password);
         }
     }
 }
