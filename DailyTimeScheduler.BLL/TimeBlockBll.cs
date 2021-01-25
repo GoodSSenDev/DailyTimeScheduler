2122d7d31e2eec056970d967b002027f09c705dc
GoodSSenDev <yoodonggeun7@naver.com>
2021-01-25 23:50:51 +1300

    Added Dto for sending and getting a data from the frontEnd

---
+++ DailyTimeScheduler.BLL/TimeBlockBll.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyTimeScheduler.BLL
+{
+    public class TimeBlockBll
+    {
+
+
+    }
+    //Make (already decided) data passing object on Common model
+
+}
---
+++ DailyTimeScheduler.Model/ScheduleDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyTimeScheduler.Model
+{
+    /// <summary>
+    /// This class is Data Transfer Object for a sending and a responding the object to fontend.
+    /// </summary>
+    public class ScheduleDto
+    {
+        public Schedule Schedule { get; set; }
+
+        public List<TimeBlock> Timeblocks { get; set; }
+    }
+}
